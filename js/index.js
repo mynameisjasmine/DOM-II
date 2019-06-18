9cf0d4bda5c1a527c2566da310304ea99361b1aa
Jasmine <jasminecharles@hotmail.com>
2019-06-18 13:09:39 -0700

    event on navbar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+
+//Event Listener 1
+let eventnav = document.querySelector('nav');
+eventnav.addEventListener('mouseover', function(event){
+   event.target.style.color = 'blue';
+});
+
+//Event Listener 2
+let eventimage = document.getElementById('bus-img');
+eventimage.addEventListener('focus', function(event){
+    event.target.style.background = 'green';
+ });
+
+//Event Listener 3
+
+//Event Listener 4
+
+//Event Listener 5
+
+//Event Listener 6
+
+//Event Listener 7
+
+//Event Listener 8
+
+//Event Listener 9
+
+//Event Listener 10
