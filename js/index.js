2f52ff063293b3e7440964ff5d570abbe4b5516c
Jasmine <jasminecharles@hotmail.com>
2019-09-03 13:31:22 -0700

    added 3 event listeners

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-// [1] Changes all h2 elements to red when moused over
+// #1 Changes all h2 elements to red when moused over
     
 const goTag = document.querySelectorAll("h2");
 
@@ -14,4 +14,34 @@ x.addEventListener('mouseleave', function(){
     goTag[i].style.color = 'black';
 });
 });
+
+
+// #2 Changes the header picture when image is clicked
+
+const updatePicture = document.querySelector("img");
+
+updatePicture.addEventListener('click', event => {
+    updatePicture.src = 'https://mk0mexiconewsdam2uje.kinstacdn.com/wp-content/uploads/2019/06/party-bus.jpg';
+})
+
+
+// #3 A popup will alert when any key is pressed
+
+window.addEventListener('keydown', event => {
+    alert('Welcome to our website!');
+})
+
+// #4
+
+// #5
+
+// #6
+
+//#7
+
+// #8
+
+// #9
+
+// #10
 
